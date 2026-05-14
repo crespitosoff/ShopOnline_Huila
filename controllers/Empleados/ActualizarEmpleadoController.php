e8c94de6c5dc20b312f16b8c6cb45e7ad0d7a138
Jhoan Sebastián Montealegre Vargas <177492567+crespitosoff@users.noreply.github.com>
2026-05-13 20:54:03 -0500

    Fix redirect paths in actualizar empleado controller

--- controllers/Empleados/ActualizarEmpleadoController.php
+++ controllers/Empleados/ActualizarEmpleadoController.php
@@ -8,16 +8,16 @@ $salario = trim($_POST['salario'] ?? '');
 
 if (empty($id) || empty($nombre) || empty($id_cargo) || !is_numeric($salario)) {
     http_response_code(400);
-    header("Location: ../views/empleados_editar.php?id=$id&error=400");
+    header("Location: ../../views/empleados_editar.php?id=$id&error=400");
     exit;
 }
 
 $objEmpleado = new Empleado();
 if ($objEmpleado->actualizar($id, $nombre, $id_cargo, $salario)) {
     http_response_code(200);
-    header("Location: ../views/empleados_listar.php?success=update");
+    header("Location: ../../views/empleados_listar.php?success=update");
 } else {
     http_response_code(500);
-    header("Location: ../views/empleados_editar.php?id=$id&error=1");
+    header("Location: ../../views/empleados_editar.php?id=$id&error=1");
 }
 exit;
